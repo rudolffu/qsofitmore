494df39e7dd6efbfad65be4d906795912d6d14e8
Yuming Fu <fuympku@outlook.com>
2021-02-21 16:41:36 +0800

    added necessary files

---
+++ qsofitmore/extinction.py
@@ -0,0 +1,86 @@
+from astropy.io import fits
+import numpy as np
+import re
+# import matplotlib.pyplot as plt
+import os
+# import importlib
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+
+def wang2019(wave, ebv, waveunit=u.AA, Rv=3.1):
+    """wang2019(self, wave, ebv, waveunit=u.AA, Rv=3.1):
+    Extinction function given by the law from Wang & Chen (2019).
+    Parameters
+    ----------
+    wave : numpy.ndarray (1-d) 
+        Wavelengths.
+    ebv : float
+        E(B-V) in magnitude.
+    waveunit : astropy.units.Quantity
+        Unit of the wavelengths.
+    Rv : float
+        Default: 3.1.
+    Returns
+    -------  
+    Alam : numpy.ndarray (1-d)
+        Extinction in magnitudes. 
+    Paper information
+    -------  
+    bibcode: 2019ApJ...877..116W
+    doi: 10.3847/1538-4357/ab1c61
+    adsurl: https://ui.adsabs.harvard.edu/abs/2019ApJ...877..116W/abstract
+    """
+
+    Av = Rv * ebv
+    uwave = wave * waveunit
+    uwave = uwave.to(u.micron)
+    Yf = 1/uwave.value - 1.82
+    Alam_over_Av = 1.0 + 0.7499 * Yf - 0.1086 * Yf ** 2 - \
+        0.08909 * Yf ** 3 + 0.02905 * Yf ** 4 + 0.01069 * \
+        Yf ** 5 + 0.001707 * Yf ** 6 - 0.001002 * Yf ** 7
+    if uwave.value.max() >= 1.0:
+        mask = uwave.value >= 1.0
+        Alam_over_Av[mask] = 0.3722 * uwave.value[mask] ** (-2.070)
+    Alam = Alam_over_Av * Av
+    return Alam
+
+        
+def redden(Alam, flux):
+    """
+    Apply extinction to flux values.
+    Parameters
+    ----------
+    Alam : numpy.ndarray (1-d)
+        Extinction in magnitudes. 
+    flux : numpy.ndarray
+        Flux values.
+    Returns
+    -------
+    flux_reddened : numpy.ndarray (1-d)
+        Flux values with extinction applied.
+    """
+
+    flux_ratio = 10. ** (-0.4 * Alam)
+    flux_reddened = flux * flux_ratio
+    return flux_reddened
+
+
+def deredden(Alam, flux):
+    """
+    Remove extinction from flux values.
+    Parameters
+    ----------
+    extinction : numpy.ndarray (1-d)
+        Extinction in magnitudes.
+    flux : numpy.ndarray
+        Flux values.
+    Returns
+    -------
+    flux_dereddened : numpy.ndarray (1-d)
+        Flux values with extinction removed.
+    """
+
+    flux_ratio = 10. ** (0.4 * Alam)
+    flux_dereddened = flux * flux_ratio
+    return flux_dereddened
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="qsofitmore", 
+    version="0.0.1",
+    author="Yuming Fu",
+    author_email="fuympku@outlook.com",
+    description="A wrapper of PyQSOFit for customization",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rudolffu/qsofitmore",
+    packages=setuptools.find_packages(),
+    # package_data={'qsofitmore': ['data/pca/*.fits']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE V3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
