8a1dc3f25c34264fb93cfafac5d971052e1fd9d8
Yuming Fu <fuympku@outlook.com>
2021-03-08 22:09:21 +0800

    added J2000 designation function to auxmodule.py

--- qsofitmore/auxmodule.py
+++ qsofitmore/auxmodule.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-
+from astropy import units as u
+from astropy.coordinates import SkyCoord
 
 # Return LaTeX name for a line / complex name
 def texlinename(name) -> str:
@@ -16,3 +17,12 @@ def texlinename(name) -> str:
     else:
         tname = name
     return tname
+
+
+def designation(ra, dec) -> str:
+    c = SkyCoord(ra=ra*u.degree, 
+                 dec=dec*u.degree,frame='icrs')
+    srahms = c.ra.to_string(unit=u.hourangle, sep='', precision=2, pad=True)
+    sdecdms = c.dec.to_string(sep='', precision=1, alwayssign=True, pad=True)
+    newname = 'J'+srahms+sdecdms
+    return newname
